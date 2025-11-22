c74bdd3688beec6bb657b320f038ad96ba30b03e
Suetin Svyatoslav <suetin_sk_0823@1511.ru>
2025-11-22 11:50:29 +0300

    Модели C++ привязаны к qml.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "src/models/schoolmodel.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
+
+    SchoolModel *schoolModel = new SchoolModel(&engine);
+    engine.rootContext()->setContextProperty("schoolModel", schoolModel);
+
     engine.loadFromModule("timesheet", "Main");
-    if (engine.rootObjects().isEmpty()) {
+    if (engine.rootObjects().isEmpty())
+    {
         qDebug() << "Failed to load QML!";
         return -1;
     }
